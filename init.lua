af08bea28793c681b5a384a34440ef595a984cfe
faintsnov <103983473+faintsnov@users.noreply.github.com>
2022-11-04 18:15:21 +0900

    for steam workshop

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 
-
+-- fix wand capacity will increase when putting in and out with bank
 ModLuaFileAppend("mods/noita-together/files/append/co_op_mail.lua", "mods/together-fix/files/append/co_op_mail.lua")
